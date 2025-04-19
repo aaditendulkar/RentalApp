cbd8e871179a305f1a52efd0e130e969688a9cbf
Aadi <aaditendulkar240205@gmail.com>
2025-04-19 20:06:01 +0530

    back sort

--- backend/user_backend/server.js
+++ backend/user_backend/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const connectDB = require("./config/db");
+const connectDB = require("../config/db");
 const cors = require("cors");
 
 // 🔍 STEP TRACKER LOGS
@@ -26,4 +26,4 @@ app.use("/api/auth", require("./routes/authRoutes"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
-const connectDB = require('./config/db');
+const connectDB = require('../config/db');
